a48e646bb2700f6f03d01f2beccd377d1163ac33
amrita222sahu <sahuamrita135@gmail.com>
2026-02-18 02:38:23 -0800

    Add files via upload

---
+++ ass1 part A Q7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+float C,F ;
+cin>>C;
+ F =(9.0/5*C)+32;
+cout<<F;
+return 0;
+
+
+}
---
+++ ass1 partA Q5.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std ;
+int main (){
+ int n ;
+ cin>>n;
+ cout <<"square="<<n*n;
+ return 0 ;
+
+
+
+
+
+
+}
