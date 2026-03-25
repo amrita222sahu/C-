93cf6e86e23a170518144e4537c008b145b1c832
amrita222sahu <sahuamrita135@gmail.com>
2026-03-25 15:22:58 +0530

    Add files via upload

---
+++ ass 2 part A Q1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the number:";
+    cin>>num;
+
+    if(num>=0)
+        cout<<"positive:";
+    else
+        cout<<"negative:";
+    return 0;
+
+    }
