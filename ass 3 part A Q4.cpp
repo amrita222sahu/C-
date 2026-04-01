d1efda6143537acb405d56d0ad3fb9c35ab53e3b
amrita222sahu <sahuamrita135@gmail.com>
2026-04-01 15:06:26 +0530

    Add files via upload

---
+++ ass 3 part A Q4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+    int main(){
+
+    int n,i=1,sum=0;
+    cin>>n;
+    while(i<=n){
+        sum+=i;
+        i++;
+    }
+    cout<<"sum="<<sum;
+    return 0;
+
+
+
+    }
