eac84afae846a2e0a784b822afd639c7e8141369
amrita222sahu <sahuamrita135@gmail.com>
2026-05-07 14:43:55 +0530

    Add files via upload

---
+++ ass 3 part B Q11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int i,j,sum;
+
+ for(i=1;i<=500;i++){
+    sum=0;
+
+    for(j=1;j<i;j++){
+        if(i%j==0){
+            sum+=j;
+        }
+    }
+    if(sum==i){
+        cout<<i<<"";
+    }
+ }
+return 0;
+
+}
